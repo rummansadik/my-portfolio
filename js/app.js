8d7a648ac2f8ef527baeed52777e4594c1e22f3b
Rumman Sadik <rummansadik@gmail.com>
2020-08-29 04:14:47 +0600

    made some changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // ----------------- SmoothScroll -------------------
 var scroll = new SmoothScroll('a[href*="#"]',{
     speed: 600
@@ -10,4 +11,14 @@ $(function () {
       var $nav = $(".fixed-top");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
+});
+
+
+// ========================================================================= //
+//  Typed Js
+// ========================================================================= //
+var typed = new Typed('.element', {
+  strings: ["Rumman Sadik", "a Designer.", "a Developer.", "a Freelancer."],
+  typeSpeed: 100,
+  loop: true,
 });
