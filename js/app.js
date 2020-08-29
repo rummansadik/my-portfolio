4225095f5ed764d94630a5e854a72b97247f70f8
Rumman Sadik <rummansadik@gmail.com>
2020-08-29 19:06:01 +0600

    added js animations

--- js/app.js
+++ js/app.js
@@ -22,3 +22,32 @@ var typed = new Typed('.element', {
   typeSpeed: 100,
   loop: true,
 });
+
+// ========================================================================= //
+//  Porfolio isotope and filter
+// ========================================================================= //
+
+// external js: isotope.pkgd.js
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+});
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.active').removeClass('active');
+    $( this ).addClass('active');
+  });
+});
+  
